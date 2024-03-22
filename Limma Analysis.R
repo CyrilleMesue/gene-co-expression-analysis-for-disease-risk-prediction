01a748f4c74bc45a9a884f64693347f99befdba3
CyrilleMesue <cyrillemesue@gmail.com>
2024-03-22 13:55:22 +0300

    completed project

--- Limma Analysis.R
+++ Limma Analysis.R
@@ -14,42 +14,94 @@ library(sva)
 library(ggrepel)
 library(patchwork)
 
-############################################################
-### Load Data and Metadata
-############################################################
-expression_data <- read.csv("data1/GSE152004_norm_data.csv", header=T, row.names=1)
-metadata<-read.csv("data1/GSE152004_metadata.csv", row.names = 1)
+# define function
+perform_DGE_analysis <- function(input_expression_data_path, 
+                                 metadata_path, 
+                                 output_path, 
+                                 normalize_with_deseq2) {
+  # Load Data and Metadata
+  expression_data <- read.csv(input_expression_data_path, header = TRUE, row.names = 1)
+  metadata <- read.csv(metadata_path, row.names = 1)
+  row.names(metadata) <- colnames(expression_data)
+  
+  # expression_data[is.na(expression_data)] <- 0
+  
+  if (normalize_with_deseq2) {
+    dds <- DESeqDataSetFromMatrix(countData = round(expression_data), colData = metadata, design = ~Type)
+    dds <- DESeq(dds)
+    a <- varianceStabilizingTransformation(dds)
+    b <- getVarianceStabilizedData(dds)
+    b1 <- rowVars(b)
+    q00_wpn <- quantile(rowVars(b))
+    expression_data <- b[b1 > q00_wpn, ]
+  }
+  
+  
+  # Compute coefficient of variation (CV) of each gene
+  expression_data <- data.frame(expression_data)
+  expression_data$CV <- apply(expression_data, 1, function(x) sd(x) / mean(x) * 100)
+  
+  # Select genes based on CV greater than 4%
+  expression_data <- expression_data[expression_data$CV > 4, ]
+  expression_data <- subset(expression_data, select = -CV)
+  
+  # Use Limma package for DEG analysis for normalized GEO dataset 
+  contrast.mat <- matrix(c(1, -1), ncol = 1)
+  dimnames(contrast.mat) <- list(c('asthmatic', 'healthy control'), "Diff")
+  sample <- metadata$Type
+  design.mat <- model.matrix(~0 + sample)
+  colnames(design.mat) <- levels(sample)
+  fit <- lmFit(expression_data, design.mat)
+  fit2 <- contrasts.fit(fit, contrast.mat)
+  fit3 <- eBayes(fit2)
+  deg1 <- topTable(fit3, n = Inf, coef = 1, adjust.method = "BH")
+  DEG1 <- as.data.frame(deg1)
+  
+  # Write results to output_path
+  write.csv(DEG1, output_path)
+}
 
-############################################################
-### Filtering Dataset Based on Coefficient of Variation
-############################################################
-# compute  coefficient of variation(CV) of each gene
-expression_data$CV<-apply(expression_data,1, function(x) sd(x) / mean(x) * 100) 
-# Select genes based on CV greater than 4 % 
-expression_data<-expression_data[expression_data$CV > 4,]
-expression_data <- subset(expression_data, select = -CV)
+# Define a list of parameter combinations
+parameter_combinations <- list(
+  list(
+    input_expression_data_path = "data1/GSE201955_norm_data.csv",
+    metadata_path = "data1/GSE201955_metadata.csv",
+    output_path = "data2/GSE201955_Limma_Results.csv",
+    normalize_with_deseq2 = FALSE
+  ),
+  list(
+    input_expression_data_path = "data1/GSE152004_norm_data.csv",
+    metadata_path = "data1/GSE152004_metadata.csv",
+    output_path = "data2/GSE152004_Limma_Results.csv",
+    normalize_with_deseq2 = TRUE
+  ),
+  list(
+    input_expression_data_path = "data1/GSE67472_norm_data.csv",
+    metadata_path = "data1/GSE67472_metadata.csv",
+    output_path = "data2/GSE67472_Limma_Results.csv",
+    normalize_with_deseq2 = FALSE
+  ),
+  list(
+    input_expression_data_path = "data1/GSE58434_norm_data.csv",
+    metadata_path = "data1/GSE58434_metadata.csv",
+    output_path = "data2/GSE58434_Limma_Results.csv",
+    normalize_with_deseq2 = TRUE
+  ),
+  list(
+    input_expression_data_path = "data1/GSE69683_norm_data.csv",
+    metadata_path = "data1/GSE69683_metadata.csv",
+    output_path = "data2/GSE69683_Limma_Results.csv",
+    normalize_with_deseq2 = FALSE
+  )
+  # Add more parameter combinations as needed
+)
 
-#Normalize with DESeq using variance stabilizingg transfomation
-expression_data[is.na(expression_data)] <- 0
-dds <- DESeqDataSetFromMatrix(countData=round(expression_data), colData = metadata , design = ~Type)
-dds <- DESeq(dds)
-a <- varianceStabilizingTransformation(dds)
-b <- getVarianceStabilizedData(dds)
-b1 <- rowVars(b)
-q00_wpn <- quantile( rowVars(b)) 
-expression_data <- b[ b1 > q00_wpn, ]
-
-# Use Limma package for DEG analysis for normalized GEO dataset 
-contrast.mat<-matrix(c(1,-1), ncol = 1)
-dimnames(contrast.mat)<-list(c('asthmatic', 'healthy control'), "Diff")
-sample<-metadata$Type
-design.mat<-model.matrix(~0+sample)
-colnames(design.mat)<-levels(sample)
-fit<-lmFit(expression_data, design.mat)
-fit2<-contrasts.fit(fit, contrast.mat)
-fit3<-eBayes(fit2)
-deg<-topTable(fit3) # top ranked DEGs
-deg1 <- topTable(fit3, n=Inf, coef=1,adjust.method="BH" )
-DEG1 <- as.data.frame(deg1)
-write.csv(DEG1, "data2/GSE67472_Lima_Results.csv")
-
+# Iterate over parameter combinations and run differential gene expression analysis
+for (params in parameter_combinations) {
+  perform_DGE_analysis(
+    input_expression_data_path = params$input_expression_data_path,
+    metadata_path = params$metadata_path,
+    output_path = params$output_path,
+    normalize_with_deseq2 = params$normalize_with_deseq2
+  )
+}
--- README.md
+++ README.md
@@ -1,4 +1,53 @@
-Downloads:
+# Genomic Data Preparation for Machine Learning
+This project is a reproduction attemption of the paper [https://www.nature.com/articles/s41598-023-35866-2](https://www.nature.com/articles/s41598-023-35866-2) on asthma disease risk production. The data has evolved from the data used in the paper so the results are very different. One thing that is common between the paper and this work is that the results poor and ML cannot yet be used in such a situation to predict asthma disease. 
+
+
+## Installation
+
+Create Environment
+
+```sh
+conda create -n envname python=3.11 -y
+```
+```sh
+conda activate envname
+```
+
+Install all packages
+```sh
+pip install -r requirements.txt
+```
+
+## Download Data
 - run this script
+```
 cd disease-risk-prediction-from-gene-co-expression/downloads
 ./downloads
+
+cd ../
+mkdir data1 data2
+```
+
+# For R Scripts
+Install the R packages in the R installations.txt file.
+
+## License
+MIT     
+**Free Software, Hell Yeah!**
+
+
+### Contributors 
+
+<table>
+  <tr>
+    <td align="center"><a href="https://github.com/CyrilleMesue"><img src="https://avatars.githubusercontent.com/CyrilleMesue" width="100px;" alt=""/><br /><sub><b>Cyrille M. NJUME</b></sub></a><br /></td>
+  </tr>
+</table>
+
+### References 
+
+[1] Eskezeia Y. Dessie et al: [https://www.nature.com/articles/s41598-023-35866-2](https://www.nature.com/articles/s41598-023-35866-2)
+
+### Contact
+
+For any feedback or queries, please reach out to [cyrillemesue@gmail.com](mailto:cyrillemesue@gmail.com).
